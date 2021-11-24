88f5b7d2a1ce88c7435ceba736814c10c805ebb4
ybouz2 <94191027+ybouz2@users.noreply.github.com>
2021-11-24 21:21:54 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var klikMij = document.querySelector("header nav img:first-of-type ");
+
+var change = document.querySelector("header nav ul")
+var nav = document.querySelector("header nav")
+
+klikMij.addEventListener("click", function() {
+  change.classList.toggle("toon")
+  nav.classList.toggle("toon")
+});
